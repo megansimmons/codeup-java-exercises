2455c14f66dda68047e0e998e01c9ea49b177be2
megansimmons <simmons.megan584@gmail.com>
2019-03-20 14:30:55 -0500

    most bonus work done

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -7,38 +7,52 @@ public class ConsoleExercises {
         System.out.format("The value of pi is approximately %.2f.", pi);
 
         Scanner scanner = new Scanner(System.in);
-        Scanner secondScanner = new Scanner(System.in);
+        scanner.useDelimiter("\n");
+//
+//
+//        System.out.println("\nEnter an integer: ");
+//
+//        int userInput = scanner.nextInt();
+//
+//        System.out.print("You entered: --> \"" + userInput + "\" <--");
+//
+//        System.out.print("Enter 3 words: ");
+//
+//        String userInputOne = scanner.next();
+//
+//
+//        String userInputTwo = scanner.next();
+//
+//
+//        String userInputThree = scanner.next();
+//        scanner.nextLine();
+//
+//
+//
+//        System.out.format("%s \n %s \n %s", userInputOne, userInputTwo, userInputThree);
+//
+//
+//
+//        System.out.println("\nEnter a sentence");
+//        String userSentence = scanner.nextLine();
+//        System.out.println(userSentence);
 
 
-        System.out.println("\n Enter an integer: ");
+        System.out.println("\nEnter the length, width, and height of your classroom.");
+        double userLength = scanner.nextDouble();
+        double userWidth = scanner.nextDouble();
 
-        int userInput = scanner.nextInt();
-
-        System.out.print("You entered: --> \"" + userInput + "\" <--");
-
-        System.out.print("Enter 3 words: ");
-
-        String userInputOne = scanner.next();
-
-//        System.out.print("Enter another word: ");
-
-        String userInputTwo = scanner.next();
-
-//        System.out.print("Enter a third word: ");
-
-        String userInputThree = scanner.next();
-
-        System.out.println(userInputOne + userInputTwo + userInputThree);
-// iamtired
-
-        System.out.format("%s \n %s \n %s", userInputOne, userInputTwo, userInputThree);
-
-        Scanner scanner2 = new Scanner(System.in);
-
-        System.out.println("\nEnter a sentence");
-        String userSentence = scanner2.nextLine();
-        System.out.println(userSentence);
-
+        double userHeight = scanner.nextDouble();
+//        int userLength = scanner.nextInt();
+//        int userWidth = scanner.nextInt();
+        System.out.format("%s \n %s \n", userLength, userWidth);
+        double area = userLength * userWidth;
+        System.out.println(area);
+        System.out.format("The area of the room is: %s", area);
+        double perimeter = 2 * (userLength + userWidth);
+        System.out.format("\n The perimeter of the room is %s", perimeter);
+        double volume = area * userHeight;
+        System.out.format("\n The volume of the room is %s", volume);
 
 
 
