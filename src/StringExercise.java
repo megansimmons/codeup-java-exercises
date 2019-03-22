7550a2fc07451c69ab6e9c84d38732d1c7ff4150
megansimmons <simmons.megan584@gmail.com>
2019-03-21 20:45:45 -0500

    StringExercise complete!

---
+++ src/StringExercise.java
@@ -0,0 +1,76 @@
+public class StringExercise {
+    public static void main(String[] args) {
+
+// INTRO TO STRINGS LECTURE
+
+//        String cheese = "cheese";
+//        System.out.println(cheese);
+//        char letter = cheese.charAt(1);
+//        System.out.println(letter);
+//        int index = cheese.indexOf('s');
+//        System.out.println(index);
+//
+//
+//
+//        System.out.println(cheese.toUpperCase().charAt(1));//H
+//        System.out.println(cheese.substring(2).toUpperCase());
+//
+//        if (cheese.equalsIgnoreCase("CHEESE"))
+//            System.out.println("snap");
+//        else
+//            System.out.println("doh");
+//
+//        System.out.println(cheese.charAt(cheese.length() - 1));
+
+
+// OTHER COMMON METHODS TO USE ON STRINGS (not all of them)
+
+//        .equals()
+//        .equalsIgnoreCase()
+//        .substring( int)
+//         .substring( int,int)
+//         .toUpperCase()
+//         .toLowerCase()
+//         .indexOf()
+//         .charAt( int)
+        //.startsWith(String)
+        //.endsWith(String)
+
+
+        //  1. For each of the following output examples, create a string variable named message that contains the desired output and print it out to the console.
+//
+//  Do this with only one string variable and one print statement.
+
+//        String message = "We don't need no education";
+//        System.out.println(message);
+//        message = "We don't need no thought control";
+//        System.out.println(message);
+//        message = "Check \"this\" out!, \"s inside of \"s!";
+//        System.out.println(message);
+//        message = "In windows, the main drive is usually C:\\";
+//        System.out.println(message);
+//        message = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\! ";
+//        System.out.println(message);
+
+
+        String message = "We don't need no education" +
+
+                "\nWe don't need no thought control" +
+
+        "\nCheck \"this\" out!, \"s inside of \"s!" +
+
+        "\nIn windows, the main drive is usually C:\\" +
+
+
+        "\nI can do backslashes \\, double backslashes \\\\, \nand the amazing triple backslash \\\\\\! ";
+
+        System.out.println(message);
+
+
+
+
+
+        // DON'T WORK BELOW THIS LINE
+
+    }
+}
